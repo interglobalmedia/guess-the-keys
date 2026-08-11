8cab685f74da130922d06cf02dbf9ac135716ede
Maria Campbell <interglobalmedia@gmail.com>
2026-08-11 11:32:26 -0400

    Add modules/Div.mjs formatting and fix --colortx typo

--- modules/Div.mjs
+++ modules/Div.mjs
@@ -5,7 +5,7 @@ import getChar from './getChar.mjs';
 
 /* background related code */
 
-/* creates a div for each unicode character set used to create the background design of the application and sets  the custom css properties named --deg, --colorbg, and colortx to these divs. These properties are used in the external CSS towards the background linear-gradient created and set on each div. */
+/* creates a div for each unicode character set used to create the background design of the application and sets the custom css properties named --deg, --colorbg, and colortx to these divs. These properties are used in the external CSS towards the background linear-gradient created and set on each div. */
 class Div {
     constructor() {
         this.element = document.createElement('div')
@@ -14,7 +14,7 @@ class Div {
         this.element.style.setProperty('--deg', range(75, 230) + 'deg');
         let color = pickColor(getRandomColor());
         this.element.style.setProperty('--colorbg', color);
-        this.element.style.setProperty('--ciolortx', color)
+        this.element.style.setProperty('--colortx', color)
     }
 }
 
