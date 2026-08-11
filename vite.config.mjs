eb4a84687fc5cf561ca1740718b80e3f232c6dce
Maria Campbell <interglobalmedia@gmail.com>
2026-08-11 06:38:46 -0400

    Replace vite with vitest/config in defineConfig import
    Add test config

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
     base: '/guess-the-keys/',
     build: {
         outDir: 'docs',
         emptyOutDir: true
+    },
+    test: {
+        environment: 'jsdom'
     }
 })
